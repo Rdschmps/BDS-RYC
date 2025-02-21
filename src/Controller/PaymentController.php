2543714d2004a64802bed016ed7ec14c64613b63
JessyPiTech <j.piquerel.p@gmail.com>
2025-02-21 11:19:54 +0100

    invoice update

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
-use App\Service\CartService;
+use App\Entity\Invoice;
+use App\Form\BillingAddressType;
 use App\Repository\ArticleRepository;
+use App\Service\CartService;
 use Doctrine\ORM\EntityManagerInterface;
-use Stripe\Stripe;
 use Stripe\Checkout\Session;
+use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PaymentController extends AbstractController
 {
@@ -20,8 +22,8 @@ class PaymentController extends AbstractController
     /**
      * Affiche la page de validation du panier et du paiement
      */
-    #[Route('/checkout', name: 'checkout_page', methods: ['GET'])]
-    public function checkoutPage(CartService $cartService, ArticleRepository $articleRepository): Response
+    #[Route('/checkout', name: 'checkout_page', methods: ['GET', 'POST'])]
+    public function checkoutPage(Request $request, CartService $cartService, ArticleRepository $articleRepository): Response
     {
         $cart = $cartService->getCart();
         $total = 0;
@@ -37,10 +39,10 @@ class PaymentController extends AbstractController
                     'image' => 'default-image.jpg'
                 ];
             } else {
-                $article = $articleRepository->find($articleId);
-                if (!$article) {
-                    continue;
-                }
+            $article = $articleRepository->find($articleId);
+            if (!$article) {
+                continue;
+            }
                 $article = [
                     'id' => $article->getId(),
                     'name' => $article->getName(),
@@ -56,9 +58,20 @@ class PaymentController extends AbstractController
             ];
         }
 
+        $form = $this->createForm(BillingAddressType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $billingData = $form->getData();
+            $request->getSession()->set('billing_address', $billingData);
+
+            return $this->redirectToRoute('checkout');
+        }
+
         return $this->render('payment/checkout.html.twig', [
             'cart' => $cartItems,
-            'total' => $total
+            'total' => $total,
+            'form' => $form->createView()
         ]);
     }
 
@@ -66,9 +79,14 @@ class PaymentController extends AbstractController
      * Initialise le paiement Stripe et renvoie une session
      */
     #[Route('/checkout-session', name: 'checkout', methods: ['POST'])]
-    public function checkout(CartService $cartService, ArticleRepository $articleRepository): JsonResponse
+    public function checkout(Request $request, CartService $cartService, ArticleRepository $articleRepository): JsonResponse
     {
         Stripe::setApiKey($this->getParameter('stripe_secret_key'));
+
+        $billingData = $request->getSession()->get('billing_address');
+        if (!$billingData) {
+            return new JsonResponse(['error' => 'Adresse de facturation manquante.'], 400);
+        }
 
         $cart = $cartService->getCart();
         if (empty($cart)) {
@@ -83,9 +101,9 @@ class PaymentController extends AbstractController
                     'price' => 10.00 // 10€ par défaut
                 ];
             } else {
-                $article = $articleRepository->find($articleId);
-                if (!$article) {
-                    continue;
+            $article = $articleRepository->find($articleId);
+            if (!$article) {
+                continue;
                 }
                 $article = [
                     'name' => $article->getName(),
@@ -124,28 +142,45 @@ class PaymentController extends AbstractController
      * Page de succès après paiement
      */
     #[Route('/payment-success', name: 'payment_success')]
-    public function success(CartService $cartService, ArticleRepository $articleRepository, EntityManagerInterface $entityManager): Response
+    public function success(Request $request, CartService $cartService, ArticleRepository $articleRepository, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
         $cart = $cartService->getCart();
+        $totalAmount = 0;
+        $billingData = $request->getSession()->get('billing_address');
 
         foreach ($cart as $articleId => $quantity) {
             $article = $articleRepository->find($articleId);
-
             if ($article && $article->getStock() && $article->getStock()->getQuantity() >= $quantity) {
+                $totalAmount += $article->getPrice() * $quantity;
                 $article->getStock()->setQuantity($article->getStock()->getQuantity() - $quantity);
                 $entityManager->persist($article);
             }
         }
 
+        $invoice = new Invoice();
+        $invoice->setUser($user)
+                ->setTransactionDate(new \DateTime())
+                ->setAmount($totalAmount)
+                ->setBillingAddress($billingData['address'])
+                ->setBillingCity($billingData['city'])
+                ->setBillingPostalCode($billingData['postal_code']);
+
+        $entityManager->persist($invoice);
         $entityManager->flush();
-        $cartService->clearCart(); // Vider le panier après la mise à jour du stock
 
-        return $this->render('payment/success.html.twig');
+        $cartService->clearCart();
+
+        return $this->render('payment/success.html.twig', ['invoice' => $invoice]);
     }
-
     /**
      * Page d'annulation du paiement
      */
+
     #[Route('/payment-cancel', name: 'payment_cancel')]
     public function cancel(): Response
     {
