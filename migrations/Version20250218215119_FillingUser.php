aff93ba27d2d7231687b8b1de20d686ef7e1620f
Elouanche <elouan.chedalleux@ynov.com>
2025-02-23 23:36:00 +0100

    modif user

--- migrations/Version20250218215119_FillingUser.php
+++ migrations/Version20250218215119_FillingUser.php
@@ -19,11 +19,14 @@ final class Version20250218215119_FillingUser extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        // Ajout manuelle, mot de passe hashé au préalable avec `php -r "echo password_hash('MDP', PASSWORD_BCRYPT) . PHP_EOL;"`
         $this->addSql('INSERT INTO user (username, password, email, balance, roles) VALUES 
-            ("guilianuser", "$2y$12$jumfasi/9kVEGHIP9PQASe1tXU4PglHN3bs2MceYV7dlo4D6ykSFK", "guilian@example.com", 500.00, "[\"ROLE_USER\"]"),
-            ("guilianadmin", "$2y$12$jFDzW0N8.SAo1lvLkabsd.S7.8PeHs59Q6km/jMukLCZPbYXuI3Tq", "guilian@example.com", 500.00, "[\"ROLE_ADMIN\"]");
+            ("guilianuser", "$2y$12$jumfasi/9kVEGHIP9PQASe1tXU4PglHN3bs2MceYV7dlo4D6ykSFK", "guilian@example.com", 100.00, "[\"ROLE_USER\"]"),
+            ("guilianadmin", "$2y$12$jFDzW0N8.SAo1lvLkabsd.S7.8PeHs59Q6km/jMukLCZPbYXuI3Tq", "guilianadmin@example.com", 500.00, "[\"ROLE_ADMIN\"]");
         ');
 
+
+        // Ajout d'articles de test
         $this->addSql("INSERT INTO article (author_id, name, description, price, published_at, image_url) VALUES 
             (1, 'Bonnet', 'Bonnet en tricot épais, alliant douceur et chaleur pour un confort optimal. Son design soigné, orné du logo BDS brodé, en fait un accessoire incontournable pour l’hiver.', 9.99, NOW(), 'Bonnet.png'),
             (1, 'Casquette', 'Casquette ajustable en coton premium, légère et respirante. Sa broderie précise du logo BDS assure un rendu élégant et durable, parfait pour un look sportif et moderne.', 14.99, NOW(), 'Cap-1.png'),
@@ -34,12 +37,13 @@ final class Version20250218215119_FillingUser extends AbstractMigration
             (1, 'T-shirt', 'T-shirt en tissu technique léger, offrant respirabilité et confort optimal. Son impression durable du logo BDS garantit un look dynamique et moderne, adapté aussi bien au sport qu\'au quotidien.', 14.99, NOW(), 'Men T Shirt-1.png'),
             (1, 'Tote Bag', 'Tote bag en coton biologique, robuste et respectueux de l’environnement. Son format pratique et son design élégant avec logo BDS en font l’allié idéal pour transporter vos affaires avec style.', 12.99, NOW(), 'Tote Bag-1.png');
         ");
-
+         // Ajout d'articles de test
          $this->addSql("INSERT INTO stock (article_id, quantity) VALUES 
          (1, 200),
          (2, 20),
          (3, 30),
          (4, 50),
+        
          (5, 45),
          (6,  450),
          (7, 2000),
